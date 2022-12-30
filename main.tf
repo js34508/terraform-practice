fae16da7c4e293b363e69c6cc6d8142df787e83d
js34508 <119328089+js34508@users.noreply.github.com>
2022-12-30 14:22:49 -0600

    Test2

--- main.tf
+++ main.tf
@@ -26,3 +26,17 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
+
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.2"
+  name  =  "Jeff-net"
+
+  vcp_id  =  data.was_vcp.default.id
+  ingress_rules  =  ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_block = ["0.0.0.0/0"]
+
+  egress_rules  =  ["all-all"]
+  egress_cidr_block = ["0.0.0.0/0"]
+
+}
