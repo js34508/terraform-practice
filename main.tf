1266b6b9bd4804c617e76ec1fcc0485aab9c885c
js34508 <119328089+js34508@users.noreply.github.com>
2022-12-30 14:43:21 -0600

    err

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "security-group" {
   version = "4.16.2"
   name  =  "Jeff-net"
 
-  vpc_id  =  data.was_vpc.default.id
+  vpc_id  =  module.blog_vpc.vpc_id
   ingress_rules  =  ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
